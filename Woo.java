2089d6dc231325285e12dd74eb36a1ba6f22db37
Lisa Eng <leng3@stuy.edu>
2017-01-12 11:46:28 -0500

    updated

--- Woo.java
+++ Woo.java
@@ -3,23 +3,53 @@ import cs1.Keyboard;
 
 public class Woo {
 
-    protected static ArrayList[][] scrabbleBoard;
+    protected  String[][] scrabbleBoard;
 
     public Woo(){
-	scrabbleBoard = new ArrayList[15][15];
+	scrabbleBoard = new String[15][15];
 	populate();
     }
 
     public void populate(){
 	for (int row = 0; row < 15; row++){
 	    for (int col = 0; col < 15; col++){
-		scrabbleBoard[row][col].add("test");
+		scrabbleBoard[row][col]="| ";
 		System.out.print(scrabbleBoard[row][col]);
 	    }
-	    System.out.print("\n");
+	    System.out.print("|\n");
 	}	
     }
+
+    public void printBoard() {
+	for (int row = 0; row < 15; row++){
+	    for (int col = 0; col < 15; col++){
+		System.out.print(scrabbleBoard[row][col]);
+	    }
+	    System.out.print("|\n");
+	}
+    }
+
+    public void input(){
+	System.out.println("Please enter the word length");
+	int wordLength = Keyboard.readInt();
+	while (wordLength != 0) {
+	    System.out.println("Please input the row index.");
+	    int row = Keyboard.readInt();
+	    System.out.println("Please input the column index.");
+	    int col = Keyboard.readInt(); //try-catch later?
+	    //System.out.println("Please state the direction
+	    System.out.println("Please type the letter you want to input");
+	    String word = Keyboard.readWord();
+	    //for (int x = 0; x < word.length()
+	    scrabbleBoard[row][col] = "|" + word;
+	    wordLength -= 1;
+	}
+}
     
     public static void main (String[] args){
+	Woo scrabble = new Woo();
+	System.out.println(scrabble.scrabbleBoard);
+	scrabble.input();
+	scrabble.printBoard();	
     }
 }
