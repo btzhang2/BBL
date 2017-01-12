9872c6180c1527b32f973f1d11971e9222c60408
Lisa Eng <Lisa Eng>
2017-01-11 22:55:16 -0500

    added on Woo.java, tried making 15x15 array

--- Woo.java
+++ Woo.java
@@ -1,7 +1,25 @@
 import java.util.ArrayList;
+import cs1.Keyboard;
 
 public class Woo {
 
-    protected ArrayList scrabbleBoard;
+    protected static ArrayList[][] scrabbleBoard;
 
+    public Woo(){
+	scrabbleBoard = new ArrayList[15][15];
+	populate();
+    }
+
+    public void populate(){
+	for (int row = 0; row < 15; row++){
+	    for (int col = 0; col < 15; col++){
+		scrabbleBoard[row][col].add("test");
+		System.out.print(scrabbleBoard[row][col]);
+	    }
+	    System.out.print("\n");
+	}	
+    }
+    
+    public static void main (String[] args){
+    }
 }
