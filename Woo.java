f7e94f3b89542aadec7434fa07e8d465857afd64
brookejin <bjin@stuy.edu>
2017-01-12 21:21:01 -0500

    added first word method

--- Woo.java
+++ Woo.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import cs1.Keyboard;
 
 public class Woo{
@@ -29,6 +28,39 @@ public class Woo{
 	}
     }
 
+    public void firstWord() {
+	System.out.println("Please enter the word length");
+	int wordLength = Keyboard.readInt();
+	int wordIndex = 0;
+	System.out.println("Please type 'right' to build your word right to left or 'up' to build your word up to down");
+	String direction = Keyboard.readWord();
+	System.out.println("Please type the word you want to input");
+	String word = Keyboard.readWord();
+	boolean validWord = Dictionary.wordChecker(word);
+	if (validWord) {
+	    int row = 7;
+	    int col = 7;
+	    if (direction.equals("right")){
+		while (wordIndex < wordLength){
+		    scrabbleBoard[row][col] = "|" + word.substring(wordIndex, wordIndex+1);
+		    col+=1;
+		    wordIndex+=1;
+		}
+	    }
+	    if (direction.equals("up")){
+		while (wordIndex < wordLength){
+		    scrabbleBoard[row][col] = "|" + word.substring(wordIndex, wordIndex+1);
+		    row+=1;
+		    wordIndex+=1;
+		}
+	    }
+	}
+	else{
+	    System.out.println("Sorry, the word you input is not valid. Please type another word. \n ");
+	    firstWord();
+	}
+    }
+    
     public void input(){
 	System.out.println("Please enter the word length");
 	int wordLength = Keyboard.readInt();
@@ -36,7 +68,7 @@ public class Woo{
 	System.out.println("Please input the row index.");
 	int row = Keyboard.readInt();
 	System.out.println("Please input the column index.");
-	int col = Keyboard.readInt(); //try-catch later?
+	int col = Keyboard.readInt();
 	System.out.println("Please type 'right' to build your word right to left or 'up' to build your word up to down");
 	String direction = Keyboard.readWord();
 	System.out.println("Please type the word you want to input");
@@ -67,7 +99,8 @@ public class Woo{
     
     public static void main (String[] args){
 	Woo scrabble = new Woo();
-	System.out.println(scrabble.scrabbleBoard);
+	scrabble.firstWord();
+	scrabble.printBoard();
 	scrabble.input();
 	scrabble.printBoard();	
     }
