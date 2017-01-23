b6861c78707a28c84fbd790265217ab9555d2259
Brandontzhang <zhangtbrandon2@gmail.com>
2017-01-23 07:59:05 -0500

    Display multiply

--- Woo.java
+++ Woo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Woo{
 
     protected static String[][] scrabbleBoard = new String[16][16];
+    protected static String[][] mscrabbleBoard = new String[16][16];
     protected static ArrayList hundredPieces = new ArrayList(100);
     protected static String[] englishLetters= {"e","e","e","e","e","e","e","e","e","e","e","e","a","a","a","a","a","a","a","a","a","i","i","i","i","i","i","i","i","i","o","o","o","o","o","o","o","o","n","n","n","n","n","n","r","r","r","r","r","r","t","t","t","t","t","t","l","l","l","l","s","s","s","s","u","u","u","u","d","d","d","d","g","g","g","b","b","m","m","c","c","p","p","f","f","h","h","v","v","w","w","y","y","k","k","j","x","q","z"};
     protected static int skipCounter;
@@ -32,6 +33,31 @@ public class Woo{
 	    }
 	}	
     }
+
+    //creates a board that displays the multiply areas
+    public static void populateMultiplyBoard(){
+	//show row numbers, for row numbers 10+, the ones digis of the tens are shown
+	for (int r = 0; r < 10; r++) {
+	    mscrabbleBoard[r][0] = Integer.toString(r);
+	}
+	for (int r = 10; r < 16; r++) {
+	    mscrabbleBoard[r][0] = Integer.toString(r).substring(1);
+	}
+
+	//show col numbers, for col numbers 10+, the ones digis of the tens are shown	
+	for (int c = 0; c < 10; c++) {
+	    mscrabbleBoard[0][c] = Integer.toString(c) + "| ";
+	}
+	for (int c = 10; c < 16; c++) {
+	    mscrabbleBoard[0][c] = Integer.toString(c).substring(1) + "| ";
+	    mscrabbleBoard[0][15] = Integer.toString(15).substring(1);
+	}
+	for (int r = 1; r < 16; r++){
+	    for (int c = 1; c < 16; c++){
+		mscrabbleBoard[r][c]="|  ";
+	    }
+	}	
+    }
     
     //prints the board, including all words and the row and column indices
     public static void printBoard() {
@@ -41,6 +67,69 @@ public class Woo{
 	    }
 	    System.out.print("|\n");
 	}
+    }
+
+    public static void printBoardM() {
+	multiplyBoard();
+	for (int r = 0; r < 16; r++){
+	    for (int c = 0; c < 16; c++){
+		System.out.print(mscrabbleBoard[r][c]);
+	    }
+	    System.out.print("|\n");
+	}
+    }
+
+    //Displays which tiles can multiply points
+    public static void multiplyBoard(){
+	mscrabbleBoard[1][15] = "|3w";
+	mscrabbleBoard[2][14] = "|2w";
+	mscrabbleBoard[3][13] = "|2w";
+	mscrabbleBoard[4][12] = "|2w";
+	mscrabbleBoard[5][11] = "|2w";
+	mscrabbleBoard[6][10] = "|3L";
+	mscrabbleBoard[7][9] = "|2L";
+	mscrabbleBoard[8][8] = "|2w";
+	mscrabbleBoard[9][7] = "|2L";
+	mscrabbleBoard[10][6] = "|3L";
+	mscrabbleBoard[11][5] = "|2w";
+	mscrabbleBoard[12][4] = "|2w";
+	mscrabbleBoard[13][3] = "|2w";
+	mscrabbleBoard[14][2] = "|2w";
+	mscrabbleBoard[15][1] = "|3w";
+	
+	mscrabbleBoard[15][15] = "|3w";
+	mscrabbleBoard[14][14] = "|2w";
+	mscrabbleBoard[13][13] = "|2w";
+	mscrabbleBoard[12][12] = "|2w";
+	mscrabbleBoard[11][11] = "|2w";
+	mscrabbleBoard[10][10] = "|3L";
+	mscrabbleBoard[9][9] = "|2L";
+	mscrabbleBoard[8][8] = "|2w";
+	mscrabbleBoard[7][7] = "|2L";
+	mscrabbleBoard[6][6] = "|3L";
+	mscrabbleBoard[5][5] = "|2w";
+	mscrabbleBoard[4][4] = "|2w";
+	mscrabbleBoard[3][3] = "|2w";
+	mscrabbleBoard[2][2] = "|2w";
+	mscrabbleBoard[1][1] = "|3w";
+
+	mscrabbleBoard[15][8] = "|3w";
+	mscrabbleBoard[1][8] = "|3w";
+	mscrabbleBoard[15][4] = "|2L";
+	mscrabbleBoard[15][12] = "|2L";
+	mscrabbleBoard[1][4] = "|2L";
+	mscrabbleBoard[1][12] = "|2L";
+	mscrabbleBoard[14][6] = "|3w";
+	mscrabbleBoard[14][10] = "|3w";
+	mscrabbleBoard[2][6] = "|3w";
+	mscrabbleBoard[2][10] = "|3w";
+	mscrabbleBoard[13][7] = "|2L";
+	mscrabbleBoard[13][9] = "|2L";
+	mscrabbleBoard[3][7] = "|2L";
+	mscrabbleBoard[3][9] = "|2L";
+	mscrabbleBoard[12][8] = "|2L";
+	mscrabbleBoard[4][8] = "|2L";
+	
     }
 
     // add the English letters to hundredPieces
@@ -72,6 +161,7 @@ public class Woo{
     //main method
     public static void main (String[] args){
 	populate();
+	populateMultiplyBoard();
 	hundred(englishLetters);
 
 	System.out.println("How many players will there be?");
@@ -106,6 +196,8 @@ public class Woo{
 	}
 
 	//player 1's first turn
+	System.out.println("This displays the special tiles where point values can be multiplied. For example 2L multiplies the letter points by 2, while 3w multiplies the word points by 3.");
+	printBoardM();
 	printBoard();
 	System.out.println(player1.getName() + " moves");
 	player1.firstWord();
